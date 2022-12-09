99096b67478286bfeab77378428c6a6aa98c2c02
YixingHuang <yixing.yh.huang@fau.de>
2022-12-09 11:11:18 +0100

    Update runCMDs.py

--- runCMDs.py
+++ runCMDs.py
@@ -1,12 +1,17 @@
 import os
 import glob
 import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 # ****************************************************************
 # training
-#
-cmd1 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -train ./examples/configFiles/deepMedicBM/train/trainConfigwideAll.cfg  -dev cuda1'
-cmd2 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -test ./examples/configFiles/deepMedicBM/test/testConfig.cfg -load  ./examples/output/saved_models/HighSensitivityAllRmsPropSI/deepMedicWide1.HighSensitivityAllRmsPropSI.final.model.ckpt -dev cuda1'
+# New training
+cmd0 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -train ./examples/configFiles/deepMedicBM/train/trainConfigwideAll.cfg  -dev cuda1'
+
+#Recommend fine tuning
+cmd1 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -train ./examples/configFiles/deepMedicBM/train/trainConfigwideAll.cfg  -load  ./examples/output/saved_models/singlePathTrainingModel/deepMedicWide1.singlePathTrainingModel.final.model.ckpt -dev cuda1'
+
+#test
+cmd2 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -test ./examples/configFiles/deepMedicBM/test/testConfig.cfg -load  ./examples/output/saved_models/singlePathTrainingModel/deepMedicWide1.singlePathTrainingModel.final.model.ckpt -dev cuda1'
 
 cmds = [cmd2]
 
