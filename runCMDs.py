2bd95c7a0d2cd39f6ec5f465fe6adc6412ef92b2
YixingHuang <yixing.yh.huang@fau.de>
2023-01-12 10:35:51 +0100

    add changes
    add changes

--- runCMDs.py
+++ runCMDs.py
@@ -8,12 +8,12 @@ import os
 cmd0 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -train ./examples/configFiles/deepMedicBM/train/trainConfigwideAll.cfg  -dev cuda1'
 
 #Recommend fine tuning
-cmd1 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -train ./examples/configFiles/deepMedicBM/train/trainConfigwideAll.cfg  -load  ./examples/output/saved_models/singlePathTrainingModel/deepMedicWide1.singlePathTrainingModel.final.model.ckpt -dev cuda1'
+cmd1 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -train ./examples/configFiles/deepMedicBM/train2/trainConfigwideAll.cfg  -load  ./examples/output/saved_models/singlePathTrainingModel/deepMedicWide1.singlePathTrainingModel.final.model.ckpt -dev cuda1'
 
 #test
-cmd2 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -test ./examples/configFiles/deepMedicBM/test/testConfig.cfg -load  ./examples/output/saved_models/singlePathTrainingModel/deepMedicWide1.singlePathTrainingModel.final.model.ckpt -dev cuda1'
+cmd2 = 'py deepMedicRun -model ./examples/configFiles/deepMedicBM/model/modelConfig_wide1_deeper.cfg  -test ./examples/configFiles/deepMedicBM/test2/testConfig.cfg -load  ./examples/output/saved_models/singlePathTrainingModel/deepMedicWide1.singlePathTrainingModel.final.model.ckpt -dev cuda1'
 
-cmds = [cmd2]
+cmds = [cmd1]
 
 for cmd in cmds:
     os.system(cmd)
